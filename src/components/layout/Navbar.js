a8e08877cfbbebba5fcf5e19727d5ef06c1e0663
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-10-10 03:48:50 +0100

    [Navbar] Add Links and modify UI

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 
 const Navbar = (props) => {
@@ -8,9 +10,19 @@ const Navbar = (props) => {
     <nav className="uk-navbar-container uk-navbar-transparent uk-light"
       data-uk-navbar="mode: click">
       <div className="uk-navbar-left">
-        <a className="uk-navbar-item uk-logo" href="#">
-          {props.logoTitle}
-        </a>
+        <Link className="uk-navbar-item uk-logo uk-text-primary" to="/">
+          <img src={logo} alt="Logo" width="50px"/> {props.logoTitle}
+        </Link>
+      </div>
+      <div className="uk-navbar-right uk-margin-right">
+        <ul className="uk-navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
